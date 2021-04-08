e12a4fd1b1f697fd4a5a8e88dbfafb2a9190744e
OnlyM1ss <sojiguy@mail.ru>
2021-04-08 21:56:03 +0400

    upload repository

--- castle_web/castle_web/Repository/IVideoRepository.cs
+++ castle_web/castle_web/Repository/IVideoRepository.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using castle_web.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace castle_web.Repository
 {
     interface IVideoRepository
     {
+        Task AddVideo(VideoModel video);
+        Task DeleteVideo(Guid Url);
+        Task ChangeVideo(Guid Url);
     }
 }
--- castle_web/castle_web/Repository/VideoRepository.cs
+++ castle_web/castle_web/Repository/VideoRepository.cs
@@ -2,10 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using castle_web.Models;
 
 namespace castle_web.Repository
 {
-    public class VideoRepository
+    public class VideoRepository : IVideoRepository
     {
+        public Task AddVideo(VideoModel video)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteVideo(Guid Url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ChangeVideo(Guid Url)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
